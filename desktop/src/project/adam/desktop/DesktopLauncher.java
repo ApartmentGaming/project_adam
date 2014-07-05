9b1b6aa3bb619bb77289ebfc59641ef8cce029b6
lordzippo <cpt_wilson@live.com>
2014-07-04 22:55:20 -0500

    initial git push

---
+++ desktop/src/project/adam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package project.adam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import project.adam.game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new game(), config);
+	}
+}
---
+++ html/src/project/adam/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package project.adam.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import project.adam.game;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new game();
+        }
+}
