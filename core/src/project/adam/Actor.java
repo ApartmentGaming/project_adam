b541226f68295eb4b4242a8b901266db9a1373d7
Wilson Loggains <>
2014-07-07 10:24:08 -0500

    Added variables and basic functions

--- core/src/project/adam/Actor.java
+++ core/src/project/adam/Actor.java
@@ -6,10 +6,52 @@
 
 package project.adam;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  *
  * @author Wilson
  */
 public class Actor {
     
+    int x;
+    int y;
+    int height;
+    int width;
+    int vx;
+    int vy;
+    Texture sprite;
+    Rectangle bounds;
+    
+    public Actor(int a, int b){
+        x = a;
+        y = b;
+        width = 32;
+        height = 32;
+        
+        bounds = new Rectangle(x,y,width,height);
+             
+    }
+    
+    public void update()
+    {
+        bounds.setPosition(x,y);
+    }
+    
+    public void sprite_render(SpriteBatch batch)
+    {
+        batch.draw(sprite,x,y);
+    }
+    
+    public void simple_render(ShapeRenderer sp)
+    {
+        sp.rect(bounds.x, bounds.y, bounds.width, bounds.height);
+    }
+    
+    public boolean collision(Actor a){
+        return a.bounds.overlaps(bounds);
+    }
 }
