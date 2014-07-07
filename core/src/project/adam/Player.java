7ba923870b7c768a0fee33f0f04b1624d68215f0
Wilson Loggains <>
2014-07-07 10:24:29 -0500

    Added variables and basic functions Also added input

--- core/src/project/adam/Player.java
+++ core/src/project/adam/Player.java
@@ -3,13 +3,57 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package project.adam;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 
 /**
  *
  * @author Wilson
  */
-public class Player extends Actor{
-    
+public class Player extends Actor {
+
+    public Player(int a, int b) {
+        super(a, b);
+        vx = 2;
+    }
+
+    public void update() {
+
+        handle_Input();
+        if(moving_left) x = x - vx;
+        if(moving_right) x = x + vx;
+        super.update();
+        
+        if(x < 0)
+        {
+            x = 0;
+        }
+        if(x + width > Gdx.graphics.getWidth())
+        {
+            x = Gdx.graphics.getWidth() - width;
+        }
+    }
+
+    boolean moving_left = false;
+    boolean moving_right = false;
+
+    public void handle_Input() {
+        if (Gdx.input.isKeyPressed(Keys.A)) {
+            if (!moving_right) {
+                moving_left = true;
+            }
+        } else {
+            moving_left = false;
+        }
+        if (Gdx.input.isKeyPressed(Keys.D)) {
+            if (!moving_left) {
+                moving_right = true;
+            }
+        } else {
+            moving_right = false;
+        }
+    }
+
 }
